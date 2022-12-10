f00106b1152e46aae4fbb971fd4fbec2568f18e1
RuChik152 <chernovn152@gmail.com>
2022-12-10 23:08:42 +0300

    update function scroll all site

--- pages/index.tsx
+++ pages/index.tsx
@@ -78,14 +78,14 @@ export default function Home() {
       if(diff > 5) {
 
           window.scrollBy({top: heightWindow, behavior: 'smooth'});
-          console.log('diff-', diff < -10);
+          console.log('diff-', diff < -3);
 
 
       }
 
       if(diff < -5) {
           window.scrollBy({top: heightWindow * (-1), behavior: 'smooth'});
-          console.log('diff', diff > 10);
+          console.log('diff', diff > 3);
       }
 
       setTouchWindowPosition(null);
