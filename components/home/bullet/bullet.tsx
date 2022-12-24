f15919130853252083cd869b0a15a08665f743f1
RuChik152 <chernovn152@gmail.com>
2022-12-24 18:29:58 +0300

    new desigin

--- components/home/bullet/bullet.tsx
+++ components/home/bullet/bullet.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Image from "next/image";
 import style from "./bullet.module.scss";
 
@@ -7,15 +7,30 @@ interface BulletProps {
     text: string,
     width: number,
     height: number,
+    name: string,
+    color?: string
 }
 
 
 
 const Bullet = (props: BulletProps) : JSX.Element => {
+
+    const [config, setConfig] = useState({
+        src: props.src,
+        width: props.width,
+        height: props.height,
+        name: props.name,
+        text: props.text,
+        styleText: {
+            color: props.color ? props.color : '#FFFFFF'
+        }
+    })
+
     return (
         <div className={style.main}>
-            <Image unoptimized={true} loading={"lazy"}  className={style.main__img} src={props.src} width={props.width} height={props.height} alt="image_bullet"/>
-            <p className={style.main__text}>{props.text}</p>
+            <Image unoptimized={true} loading={"lazy"}  className={style.main__img} src={config.src} width={config.width} height={config.height} alt="image_bullet"/>
+            <p style={config.styleText} className={style.main__name}>{config.name}</p>
+            <p className={style.main__text}>{config.text}</p>
         </div>
     );
 };
--- components/home/description.tsx
+++ components/home/description.tsx
@@ -7,27 +7,13 @@ import 'react-alice-carousel/lib/alice-carousel.css';
 import bg from "../../public/images/sky.png";
 import {bulletText} from '../../lib/mock_data';
 import Bullet from "./bullet/bullet";
-
-const img_params = {
-    width: 500,
-    height: 500
-}
-
-const items = [
-    <Image key={1} src="/images/desc/demo1.jpg" alt="logo" width={img_params.width} height={img_params.height}/>,
-    <Image key={2} src="/images/desc/demo2.jpg" alt="logo" width={img_params.width} height={img_params.height}/>,
-    <Image key={3} src="/images/desc/demo3.jpg" alt="logo" width={img_params.width} height={img_params.height}/>,
-];
-
-interface BulletTextProps {
-    img: string,
-    text: string,
-}
+import bg_logo from "../../public/images/BG_BOX_TITTLE_DESCRIPTION_2.png";
 
 
-const widthHeightImg = {
-    width: 240,
-    height: 240
+const styleConfig = {
+    width: 554,
+    height: 554,
+    color: '#8B2062',
 }
 
 const Description = () => {
@@ -38,6 +24,13 @@ const Description = () => {
         // background: 'linear-gradient(220deg, rgba(255,255,255,1) 0%, rgba(179,216,233,1) 12%, rgba(70,195,255,1) 26%, rgba(204,238,255,1) 47%, rgba(48,166,236,1) 65%, rgba(23,159,250,1) 78%, rgba(166,219,255,1) 90%, rgba(0,146,255,1) 100%)',
     }
 
+    const configStyle = {
+        backgroundImage: `url(${bg_logo.src})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center center'
+    }
+
     useEffect(() => {
         const list = document.querySelector(`${style.present__item}`);
     },[])
@@ -46,13 +39,18 @@ const Description = () => {
         <div className={style.main}>
             <div style={styled} className={style.bg}></div>
             <div  id="about" className={style.wrapper__text}>
-                <h2 className={style.tittle}>Just start <em>playing</em> and <em>find</em> out my secret</h2>
-                <p className={style.desc}>We are &apos;BELIVR&apos; - a young game&apos;s developer. We present our first project and enter the gaming industry with the fantasy VR shooter &apos;Karga&apos;</p>
+                <div style={configStyle} className={style.box__tittle}>
+                    <h2 className={style.tittle}>Just start playing and find out my secret</h2>
+                    <p className={style.desc}>We are &apos;BELIVR&apos; - a young game&apos;s developer. We present our first project and enter the gaming industry with the fantasy VR shooter &apos;Karga&apos;</p>
+                </div>
+                {/*TODO delete after check*/}
+                {/*<h2 className={style.tittle}>Just start <em>playing</em> and <em>find</em> out my secret</h2>*/}
+                {/*<p className={style.desc}>We are &apos;BELIVR&apos; - a young game&apos;s developer. We present our first project and enter the gaming industry with the fantasy VR shooter &apos;Karga&apos;</p>*/}
                 <div className={style.bullet__container}>
-                    <Bullet src="/images/bullet/elements.svg" text={bulletText[0].text} width={widthHeightImg.width} height={widthHeightImg.height}/>
-                    <Bullet src="/images/bullet/karga.png" text={bulletText[1].text} width={widthHeightImg.width} height={widthHeightImg.height}/>
-                    <Bullet src="/images/bullet/hero.png" text={bulletText[2].text} width={widthHeightImg.width} height={widthHeightImg.height}/>
-                    <Bullet src="/images/bullet/Skeleton.png" text={bulletText[3].text} width={widthHeightImg.width} height={widthHeightImg.height}/>
+                    <Bullet src="/images/bullet/new/elements.png" name={bulletText[0].name} text={bulletText[0].text} width={styleConfig.width} height={styleConfig.height} color={styleConfig.color}/>
+                    <Bullet src="/images/bullet/new/hero.png" name={bulletText[1].name} text={bulletText[1].text} width={styleConfig.width} height={styleConfig.height}/>
+                    <Bullet src="/images/bullet/new/witch-transformed.png" name={bulletText[2].name} text={bulletText[2].text} width={styleConfig.width} height={styleConfig.height} color={styleConfig.color}/>
+                    <Bullet src="/images/bullet/new/skeleton-transformed.png" name={bulletText[3].name} text={bulletText[3].text} width={styleConfig.width} height={styleConfig.height} color={styleConfig.color}/>
                 </div>
             </div>
 
